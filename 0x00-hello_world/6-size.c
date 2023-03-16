43203f1beb7b79e8cfc49ff9d5fde795602d65eb
Nabilatopo11 <nabilatopo11@hotmail.com>
2023-03-16 05:53:13 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char chartype;
+int integertype;
+long int longinttype;
+long long int longlonginttype;
+float floattype;
+printf("Size of char: %ld byte\n", sizeof(chartype));
+printf("Size of int: %ld bytes\n", sizeof(integertype));
+printf("Size of long int: %ld bytes\n", sizeof(longinttype));
+printf("Size of long long int: %ld bytes\n", sizeof(longlonginttype));
+printf("Size of float: %ld bytes\n", sizeof(floattype));
+return (0);
+}
