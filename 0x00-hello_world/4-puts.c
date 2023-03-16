94f34389fe4739acc15bf6a7dbd33e0e7c2a8b55
Nabilatopo11 <nabilatopo11@hotmail.com>
2023-03-16 04:30:28 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char Mystr[] ="\"Programming is like building a multilingual puzzle";
+puts(Mystr);
+return (0);
+}
