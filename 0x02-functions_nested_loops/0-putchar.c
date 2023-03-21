1f2c21bdb52faf6aee249b229ae219836c651600
Nabilatopo11 <nabilatopo11@hotmail.com>
2023-03-21 05:00:50 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * main - check the code
- *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
+
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -15,5 +15,7 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return (0);
+
+return(0);
+
 }
