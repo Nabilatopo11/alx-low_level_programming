cee00271eb2749cc7848ceb1d9992a9cc6ad5304
Nabilatopo11 <nabilatopo11@hotmail.com>
2023-03-21 15:13:42 -0700

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* print_last_digit-print last digit of n
+* print_last_digit -print last digit of n.
 * @n : is the int to calculate the last digit.
 * Return: the last digit.
 */
 int print_last_digit(int n)
 {
 int lastdigit = n % 10;
-return lastdigit;
+return(lastdigit);
 }
