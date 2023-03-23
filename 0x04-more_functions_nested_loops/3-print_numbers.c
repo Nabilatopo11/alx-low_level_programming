3ec59b6de2701507d69b7491a8b65b21e65f599a
Nabilatopo11 <nabilatopo11@hotmail.com>
2023-03-23 04:24:32 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9.
+ */
+void print_numbers(void)
+{
+char c;
+for (c = 48; c < 58; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
