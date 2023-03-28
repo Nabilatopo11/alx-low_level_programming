433f760f6ecf691bfd2a605114a7780219aae574
nabilatopo11 <nabilatopo11@hotmail.com>
2023-03-28 05:00:44 -0700

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string.
+ *@s : string.
+ */
+void rev_string(char *s)
+{
+char a = s[0];
+int c = 0;
+int i;
+while (s[c] != '\0')
+c++;
+for (i = 0; i < c; i++)
+{
+c--;
+a = s[i];
+s[i] = s[c];
+s[c] = a;
+}
+}
+
