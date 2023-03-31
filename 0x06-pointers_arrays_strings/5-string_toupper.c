b96c890743325502601d0c86b8bd323c4f8f090b
nabilatopo11 <nabilatopo11@hotmail.com>
2023-03-31 04:32:10 -0700

    toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,12 +7,13 @@
 char *string_toupper(char *str)
 {
 int i;
-for (i = 0 ; i != '\0'; i++)
+for (i = 0 ; str[i] != '\0'; i++)
 {
-if (str[i] >= 97 && str[i] <= 122)
+if (str[i] >= 'a' && str[i] <= 'z')
+{
 str[i] -= 32;
+}
 }
 return (str);
 }
-
 
