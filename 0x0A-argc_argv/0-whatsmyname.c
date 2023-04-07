6221065131bfb45dddcb292a31f179620463dc17
nabilatopo11 <nabilatopo11@hotmail.com>
2023-04-07 07:41:11 -0700

    whatsmayname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc : the counter
+ * @argv:the string
+ *
+ * Return: Always 0 (Success)
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
