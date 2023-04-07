a94adbdf6580cbd61aa2bdf32625d5fbd0ec0d68
nabilatopo11 <nabilatopo11@hotmail.com>
2023-04-07 15:18:18 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc : the counter
+ * @argv:the string
+ *
+ * Return: Always 0 (Success)
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+int i,mul = 1;
+if (argc < 3 || argc > 3)
+{
+printf("Error\n");
+return (1);
+}
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+mul = mul * atoi(argv[i]);
+}
+printf("%d\n", mul);
+}
+return (0);
+}
+
