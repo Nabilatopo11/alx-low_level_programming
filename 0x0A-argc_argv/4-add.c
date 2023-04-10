ff02ad05a084235a1850138234dc937e810a0c6a
nabilatopo11 <nabilatopo11@hotmail.com>
2023-04-09 23:39:12 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc : the counter
+ * @argv:the string
+ *
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+int i, add = 0;
+if (argc < 2)
+printf("0\n");
+else
+{
+for (i = 1; i < argc; i++)
+{
+if (isdigit(argc))
+{
+add = add +atoi(argv[i]);
+}
+printf("%d\n", add);
+else 
+{
+printf("Error\n");
+return (1);
+}
+}
+return (0);
+}
+}
