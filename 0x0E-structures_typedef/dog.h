f7e4b071931cde84469e7e72458238c628d77fa2
nabilatopo11 <nabilatopo11@hotmail.com>
2023-04-17 06:49:20 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - this descripction about which dog
+ * @name: sdfasdf
+ * @age:  asdfas
+ * @owner:  asdf
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
